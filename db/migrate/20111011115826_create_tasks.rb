7ef8842f1c0bf879ea4fe0a7b8ecda060dc0ed29
hatone <b1006006@gmail.com>
2011-10-13 00:43:08 +0900

    投稿できてます

--- db/migrate/20111011115826_create_tasks.rb
+++ db/migrate/20111011115826_create_tasks.rb
@@ -1,11 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
-  def change
-    create_table :tasks do |t|
-      t.string :name
-      t.date :due_date
-      t.boolean :done
-
-      t.timestamps
+    def change
+        create_table :tasks do |t|
+            t.string :name
+            t.date :due_date
+            t.boolean :done, :null => false, :default => false
+            t.timestamps
+        end
     end
-  end
 end
